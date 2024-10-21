3f3230b5d07d0242bc64c6445ac5e68e38ae11a8
masakiShito <happy.engineer.life@gmail.com>
2024-10-21 22:03:43 +0900

    追加

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+// src/pages/Home.tsx
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <h2>ホームページ</h2>
+            <p>ToDoリストアプリへようこそ。</p>
+        </div>
+    );
+};
+
+export default Home;
